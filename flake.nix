269a31e431b312596605552d9be3ec3aa1938449
bhillermann <bhillermann@gmail.com>
2025-08-14 10:20:04 +1000

    Created flake with needed Python packages.

---
+++ flake.nix
@@ -0,0 +1,56 @@
+{
+  description = "trade-analysis flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { nixpkgs, ... }:
+    let
+      supportedSystems = [ "x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
+
+      forAllSystems = f: 
+	nixpkgs.lib.genAttrs supportedSystems (system: 
+	  let
+	    pkgs = nixpkgs.legacyPackages.${system};
+    
+	    pythonEnv = pkgs.python3.withPackages (p: with p; [
+	      numpy
+	      pandas
+	      openpyxl
+          beautifulsoup4
+	    ]);
+
+	  in 
+
+	    f { inherit pkgs pythonEnv system; }
+	);
+
+    in {
+      packages = forAllSystems ({ pkgs, pythonEnv, ... }: { 
+	default = pkgs.stdenv.mkDerivation {
+	  pname = "nvcr trade-analysis";
+	  version = "0.1";
+
+	  src = ./.;
+
+	  buildInputs = [ pythonEnv ];
+	  dontBuild = true;
+
+          installPhase = ''
+            mkdir -p $out/bin
+            cp $src/db-nvrmap.py $out/bin/db-nvrmap
+            cp $src/db-ensym.py $out/bin/db-ensym
+            chmod +x $out/bin/db-nvrmap
+            chmod +x $out/bin/db-ensym
+          '';
+        };
+      });
+
+      devShells = forAllSystems ({ pkgs, pythonEnv, ... }: {
+	default = pkgs.mkShell {
+	  buildInputs = [ pythonEnv ];
+	};
+      });
+    };
+}
