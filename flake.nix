c0c201b2cc3c3250684ff015e8cdd3db58929e89
bhillermann <bhillermann@gmail.com>
2025-10-30 07:05:06 +1100

    Corrected flake.nix to install all the correct python packages and geckodriver and firefox. TODO: Fix the buildphase and DRY the call for all the packages.

--- flake.nix
+++ flake.nix
@@ -7,51 +7,55 @@
 
   outputs = { nixpkgs, ... }:
     let
-      supportedSystems = [ "x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
+    supportedSystems = [ "x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
 
-      forAllSystems = f: 
-	nixpkgs.lib.genAttrs supportedSystems (system: 
-	  let
-	    pkgs = nixpkgs.legacyPackages.${system};
-    
-	    pythonEnv = pkgs.python3.withPackages (p: with p; [
-	      numpy
-	      pandas
-	      openpyxl
-          beautifulsoup4
-          thefuzz
-	    ]);
+  forAllSystems = f: 
+    nixpkgs.lib.genAttrs supportedSystems ( system: 
+	let
+	  pkgs = nixpkgs.legacyPackages.${system};
 
-	  in 
+	  pythonEnv = pkgs.python3.withPackages ( p: with p; [
+	    numpy
+	    pandas
+	    openpyxl
+	    beautifulsoup4
+	    selenium
+	    thefuzz
+	    requests
+	    lxml
+	    xlsxwriter
+	  ]);
 
-	    f { inherit pkgs pythonEnv system; }
-	);
+	in 
 
-    in {
-      packages = forAllSystems ({ pkgs, pythonEnv, ... }: { 
+	  f { inherit pkgs pythonEnv system; }
+    );
+
+  in {
+    packages = forAllSystems ( { pkgs, pythonEnv, ... }: { 
 	default = pkgs.stdenv.mkDerivation {
 	  pname = "nvcr trade-analysis";
 	  version = "0.1";
 
 	  src = ./.;
 
-	  buildInputs = [ pythonEnv ];
+	  buildInputs = [ pythonEnv pkgs.geckodriver pkgs.firefox];
 	  dontBuild = true;
 
-          installPhase = ''
-            mkdir -p $out/bin
-            cp $src/db-nvrmap.py $out/bin/db-nvrmap
-            cp $src/db-ensym.py $out/bin/db-ensym
-            chmod +x $out/bin/db-nvrmap
-            chmod +x $out/bin/db-ensym
-          '';
-        };
-      });
-
-      devShells = forAllSystems ({ pkgs, pythonEnv, ... }: {
-	default = pkgs.mkShell {
-	  buildInputs = [ pythonEnv ];
+	  installPhase = ''
+	    mkdir -p $out/bin
+	    cp $src/db-nvrmap.py $out/bin/db-nvrmap
+	    cp $src/db-ensym.py $out/bin/db-ensym
+	    chmod +x $out/bin/db-nvrmap
+	    chmod +x $out/bin/db-ensym
+	  '';
 	};
-      });
-    };
+    });
+
+    devShells = forAllSystems ({ pkgs, pythonEnv, ... }: {
+	default = pkgs.mkShell {
+	  buildInputs = [ pythonEnv pkgs.geckodriver pkgs.firefox ];
+	};
+    });
+  };
 }
